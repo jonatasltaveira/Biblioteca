e70ab3fdfe4b4f2b26f6f604028126ed57d18e8a
Jonatas Taveira <jtaveira@gmail.com>
2023-04-19 19:26:32 -0500

    Login implementado e funcional, inclusive com a opção de sair (auth/sair

--- livro/views.py
+++ livro/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from usuario.models import Usuario
 
 # Create your views here.
-def cadastrar(request):
-    return HttpResponse('Olá')
+def home(request):
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id = request.session['usuario']).nome
+        return HttpResponse(f'Olá {usuario}. <br>Tela de Home. Em construção...')
+    else:
+        return redirect('/auth/login/?status=2')
--- usuario/views.py
+++ usuario/views.py
@@ -5,7 +5,8 @@ from hashlib import sha256
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    status = request.GET.get('status')
+    return render(request, 'login.html', {'status': status})
 
 def cadastro(request):
     status = request.GET.get('status')
@@ -21,14 +22,14 @@ def valida_cadastro(request):
     if len(nome.strip()) == 0 or len(email.strip()) == 0:
         return redirect('/auth/cadastro/?status=1')
 
-    if len(senha)< 8:
+    if len(senha) < 8:
         return redirect('/auth/cadastro/?status=2')
 
     if len(usuario) > 0:
         return redirect('/auth/cadastro/?status=3')
 
     try:
-        #senha = sha256(senha.enconde()).hexdigest()
+        senha = sha256(senha.encode()).hexdigest()
         usuario = Usuario(nome=nome, email=email, senha=senha)
         usuario.save()
         return redirect('/auth/cadastro/?status=0')
@@ -39,6 +40,20 @@ def valida_cadastro(request):
 def validar_login(request):
     email = request.POST.get('email')
     senha = request.POST.get('senha')
+    senha = sha256(senha.encode()).hexdigest()
+
+    usuario = Usuario.objects.filter(email=email).filter(senha=senha)
+
+    if len(usuario) == 0:
+        return redirect('/auth/login/?status=1')
+    elif len(usuario) > 0:
+        request.session['usuario'] = usuario[0].id
+        return redirect('/livro/home/')
+
     return HttpResponse(f"{email} {senha}")
+
+def sair(request):
+    request.session.flush()
+    return redirect('/auth/login')
 
 
